09a88c21dba27000fb978e2fb2b139135c19938e
Sunny-iLink <william@ilinktechnologies.net>
2018-07-16 07:29:47 -0500

    Hello final

--- force-app/main/default/aura/HelloTom/HelloTomController.js
+++ force-app/main/default/aura/HelloTom/HelloTomController.js
@@ -5,15 +5,10 @@
 		var attLabelValue = component.get("v.lblHello");
 		var attInputValue = component.get("v.inpName");
 
-        alert("attLabelValue: " + attLabelValue);
-        alert("attInputValue: " + attInputValue);
-        
         if(attInputValue == null || attInputValue == ''){
-            alert("EMPTY: The input text is empty!");
             component.set("v.lblHello", 'Hello ...');
         }
         else{ 
-            alert("VALUE: " + "Hello " + attInputValue);
             component.set("v.lblHello", 'Hello ' + attInputValue);
         } 
 	}
